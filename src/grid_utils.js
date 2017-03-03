ef90a60fc46007058f23949e63e7a49a8f9a70e1
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-03-02 18:09:38 -0800

    Add grid_utils

---
+++ src/grid_utils.js
@@ -0,0 +1,36 @@
+function getSides(x, y, width, height) {
+  let sides = {
+    up        : [x, y - 1],
+    upRight   : [x + 1, y - 1],
+    right     : [x + 1, y],
+    downRight : [x + 1, y + 1],
+    down      : [x, y + 1],
+    downLeft  : [x - 1, y + 1],
+    left      : [x - 1, y],
+    upLeft    : [x - 1, y - 1]
+  }
+
+  // Shave off sides
+  if (x === 0) {
+    delete sides.left;
+    delete sides.upLeft;
+    delete sides.downLeft;
+  } else if (x === width) {
+    delete sides.right;
+    delete sides.upRight;
+    delete sides.downRight;
+  }
+
+  // Shave off top/bottom
+  if (y === 0) {
+    delete sides.up;
+    delete sides.upRight;
+    delete sides.upLeft;
+  } else if (y === height) {
+    delete sides.down;
+    delete sides.downRight;
+    delete sides.downLeft;
+  }
+
+  return sides
+}
