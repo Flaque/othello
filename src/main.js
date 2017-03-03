894051a3dc307ecb407bd729eb4b873e078b3123
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-03-03 15:14:21 -0800

    Add partial availability shows

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "flexboxgrid/css/flexboxgrid.min.css"
 import "./styles.css"
 import Vue from './vendor/vue.js'
+import {getSides} from './grid_utils.js'
 
 const PLAYERS = {
   BLACK: "black",
@@ -12,16 +13,24 @@ const WIDTH = 8
 const HEIGHT = 8
 
 function createBoardState() {
-  return Array.from({length: HEIGHT}, () => {
-    return Array.from({length: WIDTH}, () => {
-      return {
+
+  let state = []
+  for (let x = 0; x < WIDTH; x++) {
+    let row = []
+    for (let y = 0; y < HEIGHT; y++) {
+      row.push({
         player: PLAYERS.EMPTY,
         active: false,
-        isWhiteAvailable: true,
-        isBlackAvailable: true
-      }
-    })
-  })
+        isWhiteAvailable: false,
+        isBlackAvailable: false,
+        x: x,
+        y: y
+      })
+    }
+    state.push(row)
+  }
+
+  return state
 }
 
 var app = new Vue({
@@ -33,6 +42,10 @@ var app = new Vue({
     whiteScore: 0
   },
   methods: {
+
+    /**
+     * Places a piece. Woopdy doodah
+     */
     placePiece: function(col, event) {
       if (col.player != PLAYERS.EMPTY) {
         return // Don't do anything on a filled cell
@@ -41,6 +54,7 @@ var app = new Vue({
       col.active = true
       col.player = this.turn
 
+      // Update turn and score
       if (this.turn === PLAYERS.WHITE) {
         this.turn = PLAYERS.BLACK
         this.whiteScore++
@@ -48,6 +62,29 @@ var app = new Vue({
         this.turn = PLAYERS.WHITE
         this.blackScore++
       }
+
+      // Mark some things as available
+      let sides = getSides(col.x, col.y, 8, 8)
+      for (let key in sides) {
+        this.rows[sides[key][0]][sides[key][1]].isWhiteAvailable = true
+        this.rows[sides[key][0]][sides[key][1]].isBlackAvailable = true
+      }
+    },
+
+    /**
+     * Get the class for the square div
+     */
+    squareClass: function(col) {
+      let isAvailable = false
+      if (this.turn === PLAYERS.WHITE && col.isWhiteAvailable) {
+        isAvailable = true
+      } else if (this.turn === PLAYERS.BLACK && col.isBlackAvailable) {
+        isAvailable = true
+      }
+
+      return {
+        'available' : isAvailable
+      }
     }
-  }
+  },
 })
