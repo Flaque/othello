796817b51420e9ead304459e3f02c5b5f9b50b0b
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-03-23 08:58:33 -0700

    Change text

--- src/components/who-should-go-first.js
+++ src/components/who-should-go-first.js
@@ -3,14 +3,14 @@ import './who-should-go-first.css'
 
 const template = `
   <div class="who-should-go-first">
-    <h1> Who should go first? </h1>
+    <h1> What color would you like to be? </h1>
 
     <div class="row">
       <div class="col-xs">
-        <button class="button" v-on:click="$emit('select', false)">Their AI</button>
+        <button class="button" v-on:click="$emit('select', false)">White</button>
       </div>
       <div class="col-xs">
-        <button class="button" v-on:click="$emit('select', true)">Our AI</button>
+        <button class="button" v-on:click="$emit('select', true)">Black</button>
       </div>
     </div>
   </div>
