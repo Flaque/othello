6390c769b2013eb1838a8c07cf9c7ef6524a5f23
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-05-03 09:41:44 -0700

    Prune stuff

--- src/pruning/prune.js
+++ src/pruning/prune.js
@@ -1,4 +1,10 @@
-let _ = require('lodash')
+import _ from 'lodash'
+import {updateAvailable, getAvailable, setCell,  BLACK_FORMAT, WHITE_FORMAT, countColors} from '../grid/grid.js'
+import {gridFromString} from '../grid/utils.js'
+
+function join(array) {
+  return Object.assign({}, ...array)
+}
 
 /**
  * Returns true if item is a string
@@ -16,11 +22,12 @@ function ABPruning(graph) {
   }
 
   let val = prune(head, graph)
-  console.log(val)
+  let cost = val[0]
 
   let path = val[1]
   path.reverse()
-  console.log(path)
+
+  return {cost, path}
 }
 
 function prune(node, graph) {
@@ -38,7 +45,7 @@ function prune(node, graph) {
     }
   }
 
-  indexOfVal = 0
+  let indexOfVal = 0
   if (graph[node][3] === "Max") {
     indexOfVal = values.indexOf(_.max(values))
   } else {
@@ -50,16 +57,142 @@ function prune(node, graph) {
   return [values[indexOfVal], visited]
 }
 
+function getMoves(board, current_move) {
+  updateAvailable(board)
+  return getAvailable(board, current_move)
+}
 
-let graph = {"C":[undefined, ["A","D","E"], undefined, "Max"],
-             "A":["C", ["P","B"], undefined, "Min"],
-             "D": ["C", ["R",42], undefined, "Min"],
-             "E":["C", ["R","V"], undefined, "Min"],
-             "P":["A", [2,3], undefined, "Max"],
-             "B": ["A", [5,100], undefined, "Max"],
-             "R": ["D", [0], undefined, "Max"],
-             "T": ["E", [2,1], undefined, "Max"],
-             "V": ["E", [9,11], undefined, "Max"]
-             }
+function cordToString(point) {
+  return point.x.toString() + ',' + point.y.toString()
+}
 
-ABPruning(graph)
+function getChildMoves(board, current_move) {
+  let moves = getMoves(board, current_move)
+
+  let keys = moves.map(function({x, y}) {
+      return cordToString({x,y})
+  })
+
+  return {moves, keys}
+}
+
+function createUndefinedItems(keys) {
+  return join(keys.map(function(node) {
+    return {
+      [node] : undefined
+    }
+  }))
+}
+
+
+//
+// let graph = {"C":[undefined, ["A","D","E"], undefined, "Max"],
+//              "A":["C", ["P","B"], undefined, "Min"],
+//              "D": ["C", ["R",42], undefined, "Min"],
+//              "E":["C", ["R","V"], undefined, "Min"],
+//              "P":["A", [2,3], undefined, "Max"],
+//              "B": ["A", [5,100], undefined, "Max"],
+//              "R": ["D", [0], undefined, "Max"],
+//              "T": ["E", [2,1], undefined, "Max"],
+//              "V": ["E", [9,11], undefined, "Max"]
+//              }
+
+
+
+function makeMove(board, x, y, isBlacksMove) {
+  let board_clone = board.slice(0)
+  let format = (isBlacksMove) ? BLACK_FORMAT : WHITE_FORMAT
+  setCell(board_clone, x, y, format)
+  return board_clone
+}
+
+
+
+function createChildEndNodes(board, isBlacksMove, moves, keys) {
+
+  let scores = []
+
+  for (let {x, y} of moves) {
+
+    // Make move
+    let board_clone = board.slice(0)
+    let format = (isBlacksMove) ? BLACK_FORMAT : WHITE_FORMAT
+    setCell(board_clone, x, y, format)
+
+    let score = (isBlacksMove) //Compute board state
+      ? countColors(board_clone).black
+      : countColors(board_clone).white
+
+    scores.push(score)
+  }
+
+  return scores
+}
+
+function moveString(isBlacksMove) {
+  return (isBlacksMove) ? "black" : "white"
+}
+
+
+/**
+ * Steps:
+ * 1. Compute the children
+ * 2. Add children
+ * 3. If there's still moves left to be made, then for each child, start from step 1
+ * 4. Return current children and their children
+ */
+
+function children(board, isBlacksMove, areWeBlack, movesAvailable, parent) {
+
+  if (movesAvailable === 0) {
+    return {}
+  }
+
+  // Create children
+  let {moves, keys} = getChildMoves(board, moveString(isBlacksMove))
+
+  let graph = createUndefinedItems(keys) // Create object of keys with undefined for now
+
+  for (let {x,y} of moves) {
+    let name = cordToString({x,y})
+    let board_clone = makeMove(board, x, y, isBlacksMove)
+
+    let childs = getChildMoves(board_clone, moveString(isBlacksMove))
+
+    if (childs.keys.length === 0) {
+      console.log(childs, moveString(isBlacksMove))
+    }
+
+    // if we're reaching the end, let's calculate the leaf nodes
+    if (movesAvailable === 1) {
+      let endnodes = createChildEndNodes(board_clone, isBlacksMove, childs.moves, childs.keys)
+      graph[name] = [parent, endnodes, undefined, "Max"]
+    } else {
+      graph[name] = [parent, childs.keys, undefined, "Max"]
+    }
+
+    // Add childs children
+    graph = Object.assign({}, graph, children(board_clone, isBlacksMove, movesAvailable-1, name))
+  }
+
+  return graph
+}
+
+
+let grid = gridFromString(`********
+                          ********
+                          ********
+                          ***wb***
+                          ***bw***
+                          ********
+                          ********
+                          ********`)
+
+let {moves, keys} = getChildMoves(grid, 'black')
+let parent = {"root":[undefined, keys, undefined, "Min"]}
+
+let graph = children(grid, false, true, 1, "root")
+
+console.log(graph)
+
+console.log(Object.keys(graph).length)
